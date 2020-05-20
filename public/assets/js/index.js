7cfa446499f82db825af1253c3ece9d5673a915c
Gavin Hung <gavinkhung@gmail.com>
2020-05-20 12:15:00 -0700

    added scroll

---
+++ public/assets/js/index.js
@@ -0,0 +1,27 @@
+// smooth scroll
+$('.navbar a').on('click', function(e){
+    if(this.hash !== 'who'){
+        e.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+    
+});
+
+// scroll magic
+$(document).ready(function(){
+    let controller = new ScrollMagic.Controller();
+
+    $('.paper').each(function(){
+        new ScrollMagic.Scene({
+            triggerElement: this
+        })
+        .setClassToggle(this, 'temp')
+        .reverse(false)
+        .addIndicators()
+        .addTo(controller);
+    });
+});
