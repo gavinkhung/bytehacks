e6979631cfaf6de51b89edc63b2b92b48d0ad6c5
Gavin Hung <gavinkhung@gmail.com>
2020-08-21 21:48:52 -0700

    added recap tab

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -29,6 +29,65 @@ $('.box').ready(function(){
 // resize
 $(document).ready(function(){
     resize();
+    const genderChartElement = document.getElementById('chart1').getContext('2d');
+    const genderChart = new Chart(genderChartElement, {
+        type: 'pie',
+        data: {
+            labels: ['Female', 'Prefer not to say', 'Male'],
+            datasets: [{
+                label: '',
+                data: [42.5, 2, 55.5],
+                backgroundColor: [
+                    'rgba(86, 140, 131, 1.0)',
+                    'rgba(39, 106, 161, 1.0)',
+                    'rgba(78, 181, 164, 1.0)'
+                ],
+                borderColor: [
+                    'rgba(86, 140, 131, 1.0)',
+                    'rgba(39, 106, 161, 1.0)',
+                    'rgba(78, 181, 164, 1.0)'
+                ],
+                borderWidth: 1
+            }],
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: 'black',
+                    fontSize: 20
+                }
+            }
+        }
+    });
+
+    const gradeChartElement = document.getElementById('chart2').getContext('2d');
+    const gradeChart = new Chart(gradeChartElement, {
+        type: 'pie',
+        data: {
+            labels: ['Middle School', 'High School'],
+            datasets: [{
+                label: '',
+                data: [34.2, 65.8],
+                backgroundColor: [
+                    'rgba(132, 94, 189, 1.0)',
+                    'rgba(86, 105, 174, 1.0)'
+                ],
+                borderColor: [
+                    'rgba(132, 94, 189, 1.0)',
+                    'rgba(86, 105, 174, 1.0)'
+                ],
+                borderWidth: 1
+            }],
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: 'black',
+                    fontSize: 20
+                }
+            }
+        }
+    });
 });
 $(window).on('resize', function() {
     resize();
@@ -49,6 +108,11 @@ $("#prospectus").click(function(){
 $("#rubric").click(function(e){
     e.preventDefault();
     $('#rubric_image.ui.modal').modal('show');
+});
+
+$("#recap_pdf").click(function(e){
+    e.preventDefault();
+    $('#recap_pdf_image.ui.modal').modal('show');
 });
 
 // sign up
@@ -81,7 +145,7 @@ $("#closing").click(function(e){
     window.open("https://docs.google.com/presentation/d/1c-UlUuVOSqTN89w7SDki-Ekj7t6TyOI_srRz05jBvOc/edit?usp=sharing", "_blank");
 });
 
-//
+// svg size for IE and Firefox
 let isIE = /*@cc_on!@*/false || !!document.documentMode;
 let isFirefox = typeof InstallTrigger !== 'undefined';
 if(isIE || isFirefox){
